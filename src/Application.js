6908807ceaa4a72ab991b6bd48cc788a741a6b37
Subhodip Pal <subhodipp@chetu.com>
2022-01-22 16:16:39 +0530

    modify changes, add highscore functionality

--- src/Application.js
+++ src/Application.js
@@ -1,324 +1,107 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-// import styles from './styles';
+import {View, Text, TouchableOpacity} from 'react-native';
+import styles from './Styles';
 
 const Application = () => {
-    const [tapTapCount, setTapTapCount] = useState(0);
-    const [count, setCount] = useState('');
-    const [showSplashScreen, setShowSplashScreen] = useState(true);
-    const [showStartScreen, setShowStartScreen] = useState(true);
-    const [showTapTapScreen, setShowTapTapScreen] = useState(false);
-    const [showResultScreen, setShowResultScreen] = useState(false);
-    const [showStartCountScreen, setShowStartCountScreen] = useState(false);
-    const [isTryAgainButtonDisable, setIsTryAgainButtonDisable] = useState(false);
+  //--------------------------- Declare states ----------------
+  const [tapTapCount, setTapTapCount] = useState(0);
+  const [showStartScreen, setShowStartScreen] = useState(true);
+  const [showTapTapScreen, setShowTapTapScreen] = useState(false);
+  const [showResultScreen, setShowResultScreen] = useState(false);
+  const [highscore, setHighscore] = useState(0);
 
-    
-
-    useEffect(()  => {
-        if(showStartCountScreen) {
-            setCount(3);
-            setTimeout(() => {
-                setCount(2);
-                setTimeout(() => {
-                    setCount(1);
-                    setTimeout(() => {
-                        setCount('Go');
-                        setTimeout(() => {
-                            setCount('');
-                            setShowStartCountScreen(false);
-                            setShowTapTapScreen(true);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }
-    }, [showStartCountScreen]);
-
-    useEffect(()  => {
-        if(showTapTapScreen) {
-            setTapTapCount(0);
-            setTimeout(() => {
-                setShowTapTapScreen(false);
-                setShowResultScreen(true);
-            }, 5 * 1000);
-        }
-    }, [showTapTapScreen]);
-
-    useEffect(() => {
-        if(showResultScreen) {
-            setIsTryAgainButtonDisable(true);
-            setTimeout(() => {
-                setIsTryAgainButtonDisable(false);
-            }, 1000)
-        }
-    }, [showResultScreen])
-
-
-    //--------------- Show Splash Screen --------------
-    setTimeout(() => {
-        setShowSplashScreen(false);
-    }, 1 * 1000);
-
-    const onClickStart = () => {
-        setShowResultScreen(false);
-        setShowStartScreen(false);
-        setShowStartCountScreen(true);
+  //--------------------------- Functions / Methods ----------------
+  useEffect(() => {
+    if (showTapTapScreen) {
+      setTapTapCount(0);
+      setTimeout(() => {
+        setShowTapTapScreen(false);
+        setShowResultScreen(true);
+      }, 5 * 1000);
     }
+  }, [showTapTapScreen]);
 
-    //------------- Screens --------------
-    const SplashScreenComponent = () => (
-        <View style={styles.viewContainer}>
-            <View style={styles.center}>
-                <View style={styles.splashScreenTextContainer}>
-                    <Text style={styles.splashScreenText}>Tap Tap Go</Text>
-                </View>
-            </View>
+  const getHighscore = () => {
+    let newHighscore =
+      highscore && highscore > tapTapCount ? highscore : tapTapCount;
+    setHighscore(newHighscore);
+    return newHighscore;
+  };
+
+  const onClickStart = () => {
+    setShowResultScreen(false);
+    setShowStartScreen(false);
+    setShowTapTapScreen(true);
+  };
+
+  //--------------------------- UI Components ----------------
+  const StartScreenComponent = () => (
+    <View style={styles.viewContainer}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Tap Tap Go</Text>
+      </View>
+
+      <View style={styles.startButtonContainer}>
+        <TouchableOpacity style={styles.startButton} onPress={onClickStart}>
+          <View style={styles.buttonTextContainer}>
+            <Text style={styles.buttonText}>Start</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
+  const TapTapScreenComponent = () => {
+    return (
+      <View style={styles.taptapScreenContainer}>
+        <TouchableOpacity
+          onPress={() => setTapTapCount(tapTapCount + 1)}
+          style={styles.taptapButtonContainer}>
+          <View style={styles.taptapTextContainer}>
+            <Text style={styles.taptapText}>Tap</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const ResultScreenComponent = () => {
+    return (
+      <View style={{flex: 1, justifyContent: 'space-between'}}>
+        <View style={styles.resultComponent}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>You have tap </Text>
+            <Text style={styles.titleText}>{tapTapCount}</Text>
+            <Text style={styles.titleText}>times</Text>
+          </View>
+          <View style={styles.messageContainer}>
+            <Text style={styles.messageText}>
+              <Text style={styles.messageCount}>
+                Highscore: {getHighscore()}{' '}
+              </Text>
+            </Text>
+          </View>
         </View>
-    )
-
-    const StartScreenComponent = () => (
-        <View style={styles.viewContainer}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.titleText}>Tap Tap Go</Text>
+        <View style={{alignSelf: 'center'}}>
+          <TouchableOpacity
+            onPress={onClickStart}
+            style={styles.tryAgainButton}>
+            <View>
+              <Text style={styles.tryAgainText}>Try Again</Text>
             </View>
-
-            <View style={styles.startButtonContainer}>
-                <TouchableOpacity
-                    style={styles.startButton}
-                    onPress={onClickStart}>
-                    <View style={styles.buttonTextContainer}>
-                        <Text style={styles.buttonText}>Start</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
+          </TouchableOpacity>
         </View>
-    )
+      </View>
+    );
+  };
 
-    const StartCountScreenCmponent = () => {
-        return (
-            <View style={styles.viewContainer}>
-                <View style={styles.startCountContainer}>
-                    <Text style={styles.startCountText}>{count}</Text>
-                </View>
-            </View>
-        )
-    }
-
-    const TapTapScreenComponent = () => {
-        return (
-            <View style={styles.taptapScreenContainer}>
-                {/* <View style={styles.spaceView} /> */}
-                <TouchableOpacity onPress={() => setTapTapCount(tapTapCount+1)} style={styles.taptapButtonContainer}>
-                    <View style={styles.taptapTextContainer}>
-                        <Text style={styles.taptapText}>Tap</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-
-    const ResultScreenComponent = () => {
-        return (
-            <View style={{flex: 1, justifyContent: 'space-between',}}>
-                <View style={styles.resultComponent}>
-                    <View style={styles.titleContainer}>
-                        <Text style={styles.titleText}>Result</Text>
-                    </View>
-                    <View style={styles.messageContainer}>
-                        <Text style={styles.messageText}>you have tap <Text style={styles.messageCount}> {tapTapCount} </Text> times</Text>
-                    </View>
-                </View>
-                <View style={{alignSelf: 'center', }}>
-                    <TouchableOpacity 
-                        disabled={isTryAgainButtonDisable}
-                        onPress={onClickStart}
-                        style={styles.tryAgainButton}>
-                            <View>
-                                <Text style={styles.tryAgainText}>Try Again</Text>
-                            </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            
-        );
-    }
-
-    return showSplashScreen ? <SplashScreenComponent /> : (
-        <View style={styles.container}>
-            {showStartScreen && <StartScreenComponent />}
-            {showStartCountScreen && <StartCountScreenCmponent />}
-            {showTapTapScreen && <TapTapScreenComponent />}
-            {showResultScreen && <ResultScreenComponent />}
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      {showStartScreen && <StartScreenComponent />}
+      {showTapTapScreen && <TapTapScreenComponent />}
+      {showResultScreen && <ResultScreenComponent />}
+    </View>
+  );
+};
 
 export default Application;
-
-//---Styles
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 24,
-      backgroundColor: "#222",
-    //   backgroundColor: ''
-    },
-    viewContainer: {
-        flex: 1,
-    },
-    center: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    //--------------------- Result Container ---------------
-
-    resultComponent: {
-        borderRadius: 8,
-        borderWidth: 2,
-        borderColor: 'white',
-        marginTop: 60,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        height: '60%',
-        elevation: 5,
-        width: '95%',
-        alignSelf: 'center',
-    },
-    messageContainer: {
-        alignSelf: 'center',
-        alignItems: 'flex-end',
-
-    },
-    messageText: {
-        color: 'white',
-        fontSize: 20,
-        marginBottom: 20,
-        lineHeight: 24,
-    },
-    messageCount: {
-        color: '#FFDC0D',
-        fontSize: 22,
-        fontWeight: 'bold',
-        lineHeight: 24,
-    },    
-    tryAgainButton: {
-        backgroundColor: '#FCD71E',
-        borderRadius: 250,
-        paddingVertical: 5,
-        paddingHorizontal: 15, 
-    },
-    tryAgainText: {
-        fontSize: 16,
-        color: '#222',
-        fontWeight: 'bold',
-        paddingVertical: 10,
-        paddingHorizontal: 10,
-    },
-
-    //--------------------- TapTap Screen --------------
-    taptapText: {
-        color: '#FFDC0D',
-        fontSize: 40,
-        fontWeight: 'bold',
-    },
-    taptapScreenContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    taptapButtonContainer: {
-        borderColor: '#FFDC0D',
-        borderWidth: 4,
-        borderRadius: 250,
-        height: 120,
-        width: 120,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 100,
-    },
-
-    //--------------------- StartCount Screen -------------
-    startCountText: {
-        color: '#FFDC0D',
-        fontSize: 100,
-        fontWeight: 'bold',
-    },
-    startCountContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    //--------------------- Splash Screen --------------
-    splashScreenTextContainer: {
-        // flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
-        backgroundColor: '#FFDC0D',
-        height: 320,
-        width: 320,
-        borderRadius: 250,
-        elevation: 50,
-        borderWidth: 4,
-        borderColor: 'white',
-        opacity: 0.8,
-        
-    },
-    splashScreenText: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 50,
-        elevation: 4,
-    },
-
-    //--------------------- Title Style   --------------
-    titleContainer: {
-        alignItems: 'center',
-        alignSelf: 'center',
-        paddingVertical: 20,
-    },
-    titleText: {
-        color: 'white',
-        fontSize: 42,
-        fontWeight: 'bold',
-    },
-
-    //--------------------- Button Style ----------------
-    startButtonContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    startButton: {
-        backgroundColor: '#FBC40E',
-        padding: 10,
-        height: 100,
-        width: 100,
-        borderRadius: 250,
-        textAlign: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
-        borderWidth: 4,
-        borderColor: '#FAEEC6',
-        elevation: 5,
-    },
-    buttonTextContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonText: {
-        fontSize: 20,
-        color: '#222',
-        fontWeight: 'bold',
-    }
-});
--- src/styles.js
+++ src/styles.js
@@ -1,27 +1,179 @@
+import {StyleSheet} from 'react-native';
 
-import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 24,
-      backgroundColor: "#eaeaea"
-    },
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#111115',
+    //   backgroundColor: ''
+  },
+  viewContainer: {
+    flex: 1,
+  },
+  center: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    title: {
-      marginTop: 16,
-      paddingVertical: 8,
-      borderWidth: 4,
-      borderColor: "#20232a",
-      borderRadius: 6,
-      backgroundColor: "#61dafb",
-      color: "#20232a",
-      textAlign: "center",
-      fontSize: 30,
-      fontWeight: "bold"
-    },
+  headerContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 28,
+  },
 
-    startButotn: {
-        backgroundColor: '#222',
-        color: '#eff'
-    }
+  //--------------------- Result Container ---------------
+
+  resultComponent: {
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: 'white',
+    marginTop: 60,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '60%',
+    elevation: 5,
+    width: '95%',
+    alignSelf: 'center',
+  },
+  messageContainer: {
+    alignSelf: 'center',
+    alignItems: 'flex-end',
+    height: 100,
+  },
+  messageText: {
+    color: 'white',
+    fontSize: 24,
+    marginBottom: 10,
+    alignSelf: 'flex-end',
+  },
+  messageCount: {
+    color: '#FFDC0D',
+    fontSize: 22,
+    fontWeight: 'bold',
+    lineHeight: 24,
+  },
+  tryAgainButton: {
+    backgroundColor: '#FCD71E',
+    borderRadius: 250,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+  },
+  tryAgainText: {
+    fontSize: 16,
+    color: '#222',
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+
+  //--------------------- TapTap Screen --------------
+  taptapText: {
+    color: '#FFDC0D',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  taptapScreenContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  taptapButtonContainer: {
+    borderColor: '#FFDC0D',
+    borderWidth: 4,
+    borderRadius: 250,
+    height: 120,
+    width: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+  },
+
+  //--------------------- StartCount Screen -------------
+  startCountText: {
+    color: '#FFDC0D',
+    fontSize: 100,
+    fontWeight: 'bold',
+  },
+  startCountContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  //--------------------- Splash Screen --------------
+  splashScreenTextContainer: {
+    // flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#FFDC0D',
+    height: 320,
+    width: 320,
+    borderRadius: 250,
+    elevation: 50,
+    borderWidth: 4,
+    borderColor: 'white',
+    opacity: 0.8,
+  },
+  splashScreenText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 50,
+    elevation: 4,
+  },
+
+  //--------------------- Title Style   --------------
+  titleContainer: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingVertical: 20,
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+  },
+
+  //--------------------- Button Style ----------------
+  startButtonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  startButton: {
+    backgroundColor: '#FBC40E',
+    padding: 10,
+    height: 100,
+    width: 100,
+    borderRadius: 250,
+    textAlign: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderWidth: 4,
+    borderColor: '#FAEEC6',
+    elevation: 5,
+  },
+  buttonTextContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#222',
+    fontWeight: 'bold',
+  },
 });
+
+export default styles;
